428018fb91ae4040ba4413246ca06d39a93fe478
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-21 14:07:18 -0500

    Added begin() function to check login. Call on every secured page.

--- util.php
+++ util.php
@@ -1,4 +1,14 @@
 <?php
+// Start session
+// Should be called on every page that needs to be secured.
+function begin() {
+  start_session();
+  if (!isset($_SESSION['userid'])) {
+    header('Location: ' . 'index.php');
+    die();
+  }
+}
+
 // MySQL connection
 function connect_db() {
   $db = new mysqli("localhost", "cs487", "cs487", "cs487");
