0b8b6e4205d7cc2ba892842ac7be9838279d1253
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-21 15:28:08 -0500

    Broke out navbar, removed old.

--- menu.php
+++ menu.php
@@ -14,21 +14,11 @@ function get_count($table) {
   <head>
     <title>Chicago Cars Inventory</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+    <script src="jquery-1.10.1.min.js"></script>
   </head>
   <body>
     <div class="container">
-      <div class="navbar">
-        <div class="navbar-inner">
-          <a class="brand" href="#">Chicago Cars</a>
-          <ul class="nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="customers.php">Customers</a></li>
-            <li><a href="inventory.php">Inventory</a></li>
-            <li><a href="sales.php">Sales</a></li>
-            <li><a href="admin.php">Admin</a></li>
-          </ul>
-        </div>
-      </div>
+      <?php include('navbar.html'); ?>
       <h2>Select a section from the menu above</h2>
       <h3>Statistics</h3>
       <ul>
