027ef7f44354f3524d3fb92bfdd99d21bf3ec0b8
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-21 13:33:38 -0500

    forgot bracket

--- util.php
+++ util.php
@@ -2,6 +2,10 @@
 // MySQL connection
 function connect_db() {
   $db = new mysqli("localhost", "cs487", "cs487", "cs487");
+  if ($db->connect_errno) {
+    echo "Failed to connect to database: " . $db->connect_errno;
+    return null;
+  }
   return $db;
 }
 
