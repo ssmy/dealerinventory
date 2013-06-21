265c478a3f7e9b7b60c074b691a26bf50e997036
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-21 16:00:03 -0500

    Create customers page.

---
+++ customers.php
@@ -0,0 +1,33 @@
+<?php
+include('util.php');
+begin();
+$db = connect_db();
+make_head("Customers");
+?>
+<html>
+  <div class="container"/>
+    <?php include('navbar.html'); ?>
+    <h1>Customers</h1>
+    <table class="table table-striped table-bordered table-hover">
+      <tr>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Address</td>
+        <td>Location</td>
+        <td>Phone</td>
+        <td>Email</td>
+      </tr>
+<?php
+$res = $db->query("SELECT * FROM customers c, people p, cities ci WHERE p.personid=c.personid AND p.city=ci.cityid");
+while ($r = $res->fetch_assoc()) {
+  echo "<td>" . $r["firstname"] . "</td>";
+  echo "<td>" . $r["lastname"] . "</td>";
+  echo "<td>" . $r["address"] . "</td>";
+  echo "<td>" . $r["city"] . ", " . $r['state'] . "</td>";
+  echo "<td>" . $r["phone"] . "</td>";
+  echo "<td>" . $r["email"] . "</td>";
+  }
+  ?>
+    </table>
+  </div>
+</html>
