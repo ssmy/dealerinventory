46d27a13b2263719169f142043dd1cc76105f379
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-21 11:54:08 -0500

    Added basic index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Chicago Cars</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+  </head>
+  <body>
+    <h1>Chicago Cars Inventory Management</h1>
+    <p>Content goes here</p>
+  </body>
+</html>
