c8e52f2f11fceee1bd7989e75097cacd29fe9427
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-29 11:41:41 -0500

    Added basic ajax to customer page
    no visible output yet, but return success/failure in console.

--- addCustomer.php
+++ addCustomer.php
@@ -1,23 +1,15 @@
 <?php
 include('util.php');
 begin();
-
-make_head("Add Customer");
-?>
-  <body>
-    <div class="container">
-      <? include('navbar.html') ?>
-      <h1>Add Customer</h1>
-<?php
 $db = connect_db();
 if (isset($_POST['submit'])) {
   if ($_POST['firstname'] != "" && $_POST['lastname'] != "") {
     if ($_POST['email'] != "") {
       if ($_POST['address'] == "") {
-        echo '<div class="alert alert-error">Please enter an address</div>';
+        $return['error'] = true;
       } else {
       if ($_POST['phone'] == "") {
-        echo '<div class="alert alert-error">Please enter a phone number</div>';
+        $return['error'] = true;
       } else {
         $res = mysqli_query($db,'INSERT INTO people (firstname, lastname, email, address, cityid, phone) VALUES ("'.$_POST['firstname'].'","'.$_POST['lastname'].'","'.$_POST['email'].'","'.$_POST['address'].'",'.$_POST['city'].','.$_POST['phone'].');');
         if ($res) {
@@ -29,55 +21,21 @@ if (isset($_POST['submit'])) {
           $_POST['address']="";
           $_POST['phone']="";
           if ($res2) {
-            echo '<div class="alert alert-success">Customer successfully created</div>';
+            $return['error'] = false;
           } else {
-            echo '<div class="alert alert-error">Error creating customer</div>';
+            $return['error'] = true;
           }  
         } else {
-          echo '<div class="alert alert-error">Error creating customer</div>';
+          $return['error'] = true;
         }
       }//phone error
       }//address error
     } else {
-      echo '<div class="alert alert-error">Please enter an email</div>';
+      $return['error'] = true;
     }
   } else {
-    echo '<div class="alert alert-error">Please enter both a first and last name</div>';
+    $return['error'] = true;
   }
 }
+echo json_encode($return);
 ?>
-      <form method="post" action="addCustomer.php">
-        <input type="hidden" name="submit">
-        <input type="text" name="firstname" <? echo ((isset($_POST['firstname']) && $_POST['firstname'] != "") ? "value=".$_POST['firstname'] : "placeholder=\"First name\""); ?>><br/>
-        <input type="text" name="lastname" <? echo ((isset($_POST['lastname']) && $_POST['lastname'] != "") ? "value=".$_POST['lastname'] : "placeholder=\"Last name\""); ?>><br/>
-        <input type="text" name="email" <? echo ((isset($_POST['email']) && $_POST['email'] != "") ? "value=".$_POST['email'] : "placeholder=\"Email\""); ?>><br/>
-        <input type="text" name="address" <? echo ((isset($_POST['address']) && $_POST['address'] != "") ? "value=".$_POST['address'] : "placeholder=\"Address\""); ?>><br/>
-        City:<br />
-        <select name="city">
-          <?
-          $res3 = $db->query("SELECT * FROM cities c");
-          while ($r = $res3->fetch_assoc()) {
-            echo "<option value=".$r['cityid'].">" . $r["city"] . ", " . $r['state']  . "</option>\n";
-          }
-          ?>
-        </select><br />
-        <input type="text" name="phone" <? echo ((isset($_POST['phone']) && $_POST['phone'] != "") ? "value=".$_POST['phone'] : "placeholder=\"Phone\""); ?>><br/>
-        Contact Type:<br />
-        <select name="contacttype">
-          <?
-          $res4 = $db->query("SELECT * FROM contacttypes c");
-          while ($r = $res4->fetch_assoc()) {
-            echo "<option value=".$r['contactid'].">" . $r["contact"] . "</option>\n";
-          }
-          ?>
-        </select><br />
-        <table name="buttontable"><tr><td>
-        <button class="btn btn-large btn-primary">Add Customer</button>
-      </form></td><td>
-      <form action="customers.php"><br />
-        <button class="btn btn-large">Cancel</button>
-      </form>
-        </td></tr></table>
-    </div>
-  </body>
-</html> 
--- customers.php
+++ customers.php
@@ -31,9 +31,72 @@ while ($r = $res->fetch_assoc()) {
   }
   ?>
     </table>
-    <form action=addCustomer.php>
-      <input type="submit" value="Add Customer" class="btn btn-primary">
-    </form>
+    <script>
+      $(document).ready(function() {
+        $('#triggerAdd').click(function() {
+          $('#addModal').modal({show:true});
+        });
+
+        $('#submit').click(function() {
+          $.ajax({
+            type:     'POST',
+            url:      'addCustomer.php',
+            dataType: 'json',
+            data:     {
+              submit: 'submit',
+              firstname: $('#firstname').val(),
+              lastname: $('#lastname').val(),
+              email: $('#email').val(),
+              address: $('#address').val(),
+              city: $('#city').val(),
+              phone: $('#phone').val(),
+              contacttype: $('#contacttype').val()
+            },
+            success: function(data) {
+              console.log('success');
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+              console.log('error');
+            }
+          });
+        });
+      });
+    </script>
+    <a id="triggerAdd" class="btn btn-primary">Add new customer</a>
+    <div id="addModal" class="modal hide fade">
+      <div class="modal-header">
+        <h3>Add new customer</h3>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="addCustomer.php">
+          <input type="hidden" name="submit">
+          <input type="text" id="firstname" <? echo ((isset($_POST['firstname']) && $_POST['firstname'] != "") ? "value=".$_POST['firstname'] : "placeholder=\"First name\""); ?>><br/>
+          <input type="text" id="lastname" <? echo ((isset($_POST['lastname']) && $_POST['lastname'] != "") ? "value=".$_POST['lastname'] : "placeholder=\"Last name\""); ?>><br/>
+          <input type="text" id="email" <? echo ((isset($_POST['email']) && $_POST['email'] != "") ? "value=".$_POST['email'] : "placeholder=\"Email\""); ?>><br/>
+          <input type="text" id="address" <? echo ((isset($_POST['address']) && $_POST['address'] != "") ? "value=".$_POST['address'] : "placeholder=\"Address\""); ?>><br/>
+          City:<br />
+          <select id="city">
+            <?
+            $res3 = $db->query("SELECT * FROM cities c");
+            while ($r = $res3->fetch_assoc()) {
+            echo "<option value=".$r['cityid'].">" . $r["city"] . ", " . $r['state']  . "</option>\n";
+            }
+            ?>
+          </select><br />
+          <input type="text" id="phone" <? echo ((isset($_POST['phone']) && $_POST['phone'] != "") ? "value=".$_POST['phone'] : "placeholder=\"Phone\""); ?>><br/>
+          Contact Type:<br />
+          <select id="contacttype">
+            <?
+            $res4 = $db->query("SELECT * FROM contacttypes c");
+            while ($r = $res4->fetch_assoc()) {
+            echo "<option value=".$r['contactid'].">" . $r["contact"] . "</option>\n";
+            }
+            ?>
+          </select><br />
+          <a id="submit" class="btn btn-large btn-primary">Add Customer</a>
+        </form>
+      </div> <!-- body -->
+    </div>
   </div>
 </body>
 </html>
