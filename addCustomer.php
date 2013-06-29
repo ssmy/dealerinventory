b5495de3baf547498e4be7be44456ef89ffd9668
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-29 11:52:59 -0500

    added error messages

--- addCustomer.php
+++ addCustomer.php
@@ -7,9 +7,11 @@ if (isset($_POST['submit'])) {
     if ($_POST['email'] != "") {
       if ($_POST['address'] == "") {
         $return['error'] = true;
+        $return['msg'] = "Please enter an address";
       } else {
       if ($_POST['phone'] == "") {
         $return['error'] = true;
+        $return['msg'] = "Please enter a phone number";
       } else {
         $res = mysqli_query($db,'INSERT INTO people (firstname, lastname, email, address, cityid, phone) VALUES ("'.$_POST['firstname'].'","'.$_POST['lastname'].'","'.$_POST['email'].'","'.$_POST['address'].'",'.$_POST['city'].','.$_POST['phone'].');');
         if ($res) {
@@ -24,17 +26,21 @@ if (isset($_POST['submit'])) {
             $return['error'] = false;
           } else {
             $return['error'] = true;
+            $return['msg'] = "Error creating customer";
           }  
         } else {
           $return['error'] = true;
+          $return['msg'] = "Error creating customer";
         }
       }//phone error
       }//address error
     } else {
       $return['error'] = true;
+      $return['msg'] = "Please enter an email";
     }
   } else {
     $return['error'] = true;
+    $return['msg'] = "Please enter a first and last name";
   }
 }
 echo json_encode($return);
