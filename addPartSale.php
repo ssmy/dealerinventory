f870d1619aaca6405d604ce8ff1652973e7f14b6
= <ronald.pyka@gmail.com>
2013-06-29 21:28:52 -0500

    fixed some error checking

--- addPartSale.php
+++ addPartSale.php
@@ -6,20 +6,24 @@ if (isset($_POST['submit']) && ($_POST['action']=="add" || $_POST['action']=="up
   if ($_POST['date'] != "" && $_POST['price'] != "" && $_POST['quantity'] != "") {
     if ($_POST['customer'] != 0 && $_POST['employee'] != 0 && $_POST['part']!=0) {
       if($_POST['action']=="add"){
-        $partquan = $db->query('SELECT quantity FROM parts WHERE partid='.$_POST['part'].';');
-        if ($_POST['quantity']>$partquan){
+        $partres = $db->query('SELECT quantity FROM parts WHERE partid='.$_POST['part'].';');
+        $partquan = $partres->fetch_row();
+        if ($_POST['quantity']>$partquan[0]){
           $return['error'] = true;
           $return['msg'] = "Trying to sell more parts than are in stock";
           echo json_encode($return);
+          die();
         } else {
           $res = $db->multi_query('INSERT INTO partsales (customerid, employeeid, partid, datesold, saleprice, quantity) VALUES ('.$_POST['customer'].','.$_POST['employee'].','.$_POST['part'].',"'.$_POST['date'].'",'.$_POST['price'].','.$_POST['quantity'].'); UPDATE parts SET quantity=quantity-'.$_POST['quantity'].' WHERE partid='.$_POST['part'].';');
         }
       } else {
-        $partquan = $db->query('SELECT parts.quantity+partsales.quantity FROM parts, partsales WHERE parts.partid='.$_POST['part'].' AND partsales.saleid='.$_POST['sale'].';');
-        if ($_POST['quantity']>$partquan){
+        $partres = $db->query('SELECT parts.quantity+partsales.quantity FROM parts, partsales WHERE parts.partid='.$_POST['part'].' AND partsales.saleid='.$_POST['sale'].';');
+        $partquan = $partres->fetch_row();
+        if ($_POST['quantity']>$partquan[0]){
           $return['error'] = true;
           $return['msg'] = "Trying to sell more parts than are in stock";
           echo json_encode($return);
+          die();
         } else {
           $res = $db->multi_query('UPDATE parts SET quantity=quantity+(SELECT quantity FROM partsales WHERE saleid='.$_POST['sale'].') WHERE partid=(SELECT partid FROM partsales WHERE saleid='.$_POST['sale'].'); UPDATE partsales SET customerid='.$_POST['customer'].', employeeid='.$_POST['employee'].', vehicleid='.$_POST['vehicle'].', datesold="'.$_POST['date'].'", saleprice='.$_POST['price'].', quantity='.$_POST['quantity'].' WHERE saleid='.$_POST['sale'].'; UPDATE parts SET quantity=quantity-'.$_POST['quantity'].' WHERE partid='.$_POST['part'].';');
         }
