d03b0d652d732cd7175de5806cbe394a77689ad2
= <ronald.pyka@gmail.com>
2013-06-30 00:18:51 -0500

    more genTable work

--- genTable.php
+++ genTable.php
@@ -6,7 +6,26 @@ if(isset($_POST['table'])){
   if($_POST['table']=="parts"){
     $res = $db->query("SELECT * FROM parts p");
     while ($r = $res->fetch_assoc()) {
-      $return['contents'][] = array($r['partid'], $r['cost'], $r['name'], $r['quantity']);
+      if(is_manger()) {
+        $edit="<a href=\"#\" class=\"edit\"><i class=\"icon-edit\"></i></a>";
+        $return['contents'][] = array($r['partid'], $r['cost'], $r['name'], $r['quantity'], $edit);
+      }
+      else{
+        $return['contents'][] = array($r['partid'], $r['cost'], $r['name'], $r['quantity']);
+      }
+    }
+    $return['error'] = false;
+  }
+  if($_POST['table']=="vehicles"){
+    $res = $db->query("SELECT * FROM vehicles v, colors c, models m, makes ma, locations l, cities ci, statuses s WHERE v.colorid=c.colorid AND v.modelid=m.modelid AND m.makeid=ma.makeid AND v.locationid=l.locationid AND l.cityid=ci.cityid AND v.statusid=s.statusid");
+    while ($r = $res->fetch_assoc()) {
+      if(is_manager()) {
+        $edit="<a href=\"#\" class=\"edit\"><i class=\"icon-edit\"></i></a>";
+        $return['contents'][] = array($r['year'], $r['color'], $r['make'], $r['model'], ucwords(strtolower($r["status"])), $r["name"], $edit);
+      }
+      else{
+        $return['contents'][] = array($r['year'], $r['color'], $r['make'], $r['model'], ucwords(strtolower($r["status"])), $r["name"]);
+      }
     }
     $return['error'] = false;
   }
