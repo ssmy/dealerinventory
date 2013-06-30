8b408ba9c55a1816bd8359c35c133940a45933f5
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-30 00:01:42 -0500

    Fixed array push and db connection

--- genTable.php
+++ genTable.php
@@ -1,12 +1,14 @@
 <?
 include('util.php');
 begin();
+$db = connect_db();
 if(isset($_POST['table'])){
   if($_POST['table']=="parts"){
     $res = $db->query("SELECT * FROM parts p");
     while ($r = $res->fetch_assoc()) {
-      $return['contents'].push([$r['partid'], $r['cost'], $r['name'], $r['quantity']]);
+      $return['contents'][] = array($r['partid'], $r['cost'], $r['name'], $r['quantity']);
     }
+    $return['error'] = false;
   }
   echo json_encode($return);
 }
