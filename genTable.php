c7d890d17092aaca467944abd44b4dec7eac1d92
= <ronald.pyka@gmail.com>
2013-06-29 23:51:11 -0500

    added code to hopefully make genTable work

--- genTable.php
+++ genTable.php
@@ -1,3 +1,13 @@
 <?
 include('util.php');
+begin();
+if(isset($_POST['table'])){
+  if($_POST['table']=="parts"){
+    $res = $db->query("SELECT * FROM parts p");
+    while ($r = $res->fetch_assoc()) {
+      $return['contents'].push([$r['partid'], $r['cost'], $r['name'], $r['quantity']]);
+    }
+  }
+  echo json_encode($return);
+}
 ?>
