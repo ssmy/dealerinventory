8e603106e1e172275f268a9551a4eb8a8a273c5b
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-30 17:09:05 -0500

    added update to addCustomer

--- addCustomer.php
+++ addCustomer.php
@@ -13,24 +13,27 @@ if (isset($_POST['submit'])) {
         $return['error'] = true;
         $return['msg'] = "Please enter a phone number";
       } else {
-        $res = mysqli_query($db,'INSERT INTO people (firstname, lastname, email, address, cityid, phone) VALUES ("'.$_POST['firstname'].'","'.$_POST['lastname'].'","'.$_POST['email'].'","'.$_POST['address'].'",'.$_POST['city'].','.$_POST['phone'].');');
+        if($_POST['action']=="add"){
+          $res = $db->query('INSERT INTO people (firstname, lastname, email, address, cityid, phone) VALUES ("'.$_POST['firstname'].'","'.$_POST['lastname'].'","'.$_POST['email'].'","'.$_POST['address'].'",'.$_POST['city'].','.$_POST['phone'].');');
+        } else {
+          $res = $db->query('UPDATE people SET firstname="'.$_POST['firstname'].'", lastname="'.$_POST['lastname'].'", email="'.$_POST['email'].'", address="'.$_POST['address'].'", cityid='.$_POST['city'].', phone="'.$_POST['phone'].'" WHERE personid='.$_POST['personid'].';');
+        }
         if ($res) {
-          $personid=mysqli_insert_id($db);
-          $res2 = mysqli_query($db,'INSERT INTO customers (personid, contacttype) VALUES ('.$personid.','.$_POST['contacttype'].');');
-          $_POST['firstname']="";
-          $_POST['lastname']="";
-          $_POST['email']="";
-          $_POST['address']="";
-          $_POST['phone']="";
+          if($_POST['action']=="add"){
+            $personid=mysqli_insert_id($db);
+            $res2 = mysqli_query($db,'INSERT INTO customers (personid, contacttype) VALUES ('.$personid.','.$_POST['contacttype'].');');
+          } else {
+            $res2 = $db->query('UPDATE customers SET contacttype=' . $_POST['contacttype'] . ' WHERE customerid=' . $_POST['customerid'] . ';');
+          }
           if ($res2) {
             $return['error'] = false;
           } else {
             $return['error'] = true;
-            $return['msg'] = "Error creating customer";
+            $return['msg'] = $_POST['action'] == "add" ? "Error creating customer" : "Error updating customer";
           }  
         } else {
           $return['error'] = true;
-          $return['msg'] = "Error creating customer";
+          $return['msg'] = $_POST['action'] == "add" ? "Error creating customer" : "Error updating customer";
         }
       }//phone error
       }//address error
