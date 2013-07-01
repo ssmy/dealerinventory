5b1d596037e91c1cd88dc7e345dd1b874fd16bb3
Daniel LaGesse <dlagesse1992@gmail.com>
2013-06-30 19:08:15 -0500

    switched employees to ajax load and added update

--- employees.php
+++ employees.php
@@ -12,7 +12,8 @@ make_head("Employees");
     <div class="container">
 <?php include('navbar.html'); ?>
       <h1>Employees</h1>
-    <table class="table table-striped table-bordered table-hover">
+    <div id="dataerror" class="alert alert-error" style="display: none;"></div>
+    <table id="table" class="table table-striped table-bordered table-hover">
       <tr>
         <th>First Name</th>
         <th>Last Name</th>
@@ -20,30 +21,89 @@ make_head("Employees");
         <th>Location</th>
         <th>Phone</th>
         <th>Email</th>
-<? // TODO: Add rank, status ?>
+        <th>Rank</th>
+        <th>Status</th>
+<? if(is_manager()) { echo "<th>Edit</th>"; } ?>
       </tr>
-<?php
-$res = $db->query("SELECT * FROM employees e, people p, cities ci WHERE p.personid=e.personid AND p.cityid=ci.cityid");
-while ($r = $res->fetch_assoc()) {
-  echo "<tr>";
-  echo "<td>" . $r["firstname"] . "</td>";
-  echo "<td>" . $r["lastname"] . "</td>";
-  echo "<td>" . $r["address"] . "</td>";
-  echo "<td>" . $r["city"] . ", " . $r['state'] . "</td>";
-  echo "<td>" . $r["phone"] . "</td>";
-  echo "<td>" . $r["email"] . "</td>";
-  echo "</tr>\n";
-  }
-  ?>
     </table>
     <script>
       $(document).ready(function() {
+        function loadData() {
+          $.ajax({
+            type:     'POST',
+            url:      'genTable.php',
+            dataType: 'json',
+            data:     {
+              table: 'employees'
+            },
+            success: function(data) {
+              if (data.error == false) {
+                for (var r = 0; r < data.contents.length; r++) {
+                  $data = "";
+                  for (var c = 0; c < data.contents[r].length; c++)
+                    $data += '<td>' + data.contents[r][c] + '</td>';
+                  $('#table tr:last').after('<tr>' + $data + '</tr>');
+                }
+                $extradata = data.extra;
+                $('.edit').click(function(){
+                  editset($(this));
+                  $action = "update";
+                  $editrow=$(this);
+                });
+              } else {
+                $('#dataerror').text(data.msg);
+                $('#dataerror').attr('style', '');
+              }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+              $('#dataerror').text("Error loading data. Please refresh.");
+              $('#dataerror').attr('style', '');
+            }
+          });
+        }
+        loadData();
+<?if(!is_manager()) {?>
+      });
+    </script>
+    <script>
+      $(document).ready(function() {
+<?} else {?>
         $('#triggerAdd').click(function() {
           $('#addModal').modal({show:true});
+          $action = "add";
         });
 
+        var editrow = null;
+
+        function editset($obj){
+          $row = $obj.closest("tr")[0].cells;
+          $('#firstname').val($row[0].innerHTML);
+          $('#lastname').val($row[1].innerHTML);
+          $('#address').val($row[2].innerHTML);
+          $('#city option:contains(' + $row[3].innerHTML + ')').prop({selected: true})
+          $('#phone').val($row[4].innerHTML);
+          $('#email').val($row[5].innerHTML);
+          $('#rank option:contains(' + $row[6].innerHTML + ')').prop({selected: true})
+          $('#status option:contains(' + $row[7].innerHTML + ')').prop({selected: true})
+          $('.modal-header h3').text('Update Employee');
+          $('#submit').text("Update Employee");
+          $('#addModal').modal({show:true}); 
+          $employeeid = $extradata[$row[0].parentNode.rowIndex - 1][0];
+          $personid = $extradata[$row[0].parentNode.rowIndex - 1][1];
+          $username = $extradata[$row[0].parentNode.rowIndex - 1][2];
+          $password = $extradata[$row[0].parentNode.rowIndex - 1][3];
+          $('#user').val($username);
+          $('#pass').val($password);
+          $('#duppass').val($password);
+        }
+
         $('.reset').click(function() {
-          $('#custform')[0].reset();
+          if ($action=="add"){
+            $('#form')[0].reset();
+            $('#message').attr("style","display:none;");
+          }
+          else
+            editset($editrow);
         });
 
         $('#submit').click(function() {
@@ -53,15 +113,20 @@ while ($r = $res->fetch_assoc()) {
             dataType: 'json',
             data:     {
               submit: 'submit',
+              action: $action,
+              personid: $personid,
+              employeeid: $employeeid,
               firstname: $('#firstname').val(),
               lastname: $('#lastname').val(),
               email: $('#email').val(),
               address: $('#address').val(),
               city: $('#city').val(),
               phone: $('#phone').val(),
+              status: $('#status').val(),
               user: $('#user').val(),
               pass: $('#pass').val(),
               duppass: $('#duppass').val(),
+              oldpass: $password,
               rank: $('#rank').val(),
             },
             success: function(data) {
@@ -69,7 +134,18 @@ while ($r = $res->fetch_assoc()) {
                 $('#message').text("Employee added successfully");
                 $('#message').attr('class', 'alert alert-success');
                 $('#message').attr('style', '');
-                $('#custform')[0].reset();
+                $('#form')[0].reset();
+                $('#form').attr('style', 'display:none;');
+                $('div.modal-footer').attr('style', 'display:none;');
+                setTimeout(function() {
+                  $('#addModal').modal('toggle');
+                  $('#table tr').not(function(){if ($(this).has('th').length){return true}}).remove();
+                  loadData();
+                  $('#form').attr('style', '');
+                  $('div.modal-footer').attr('style', '');
+                  $('#message').attr('style', 'display: none;');
+                }, 2000);
+                $('#form')[0].reset();
                 } else {
                 $('#message').text(data.msg);
                 $('#message').attr('class', 'alert alert-error');
@@ -77,6 +153,7 @@ while ($r = $res->fetch_assoc()) {
               }
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
+              console.log(XMLHttpRequest);
               $('#message').text("Error adding employee");
               $('#message').attr('class', 'alert alert-error');
               $('#message').attr('style', '');
@@ -119,6 +196,16 @@ while ($r = $res->fetch_assoc()) {
             }
             ?>
           </select>
+          <br/>
+          Status:<br />
+          <select id="status">
+            <?
+            $res4 = $db->query("SELECT * FROM employeestatuses e");
+            while ($r = $res4->fetch_assoc()) {
+            echo "<option value=".$r['statusid'].">" . ucwords(strtolower($r["status"])) . "</option>\n";
+            }
+            ?>
+          </select>
         </form>
       </div> <!-- body -->
       <div class="modal-footer">
@@ -127,6 +214,7 @@ while ($r = $res->fetch_assoc()) {
         <a id="submit" class="btn btn-primary">Add employee</a>
       </div>
     </div>
+    <?}?>
   </div>
 </body>
 </html>
